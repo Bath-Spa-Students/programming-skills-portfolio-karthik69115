68cdb53495d4805c0212c4bdaadb10a392b5511c
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-10-12 00:15:56 +0400

    Update practice3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice3.py
@@ -0,0 +1,4 @@
+#Q.3 You need to make a leader board. The program needs to output the number 1 to 9 each on a seperate line followed by a dot 
+for i in range(1, 10):
+    print(i, end=',')
+    print()
