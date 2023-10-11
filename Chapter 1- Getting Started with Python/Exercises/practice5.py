498ace30d7587bd2802755acd31163b0a9440eaf
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-10-12 00:17:38 +0400

    Update practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -0,0 +1,9 @@
+#Q.5 Write four strings in different variables and print the output as one string.
+# Four string in different variables
+string1 = "Hello"
+string2 = "World"
+string3 = "!"
+string4 = "how are you?"
+# concatenate the string and print the out put as one string
+output_string = string1 + string2 + string3 + string4
+print(output_string)
