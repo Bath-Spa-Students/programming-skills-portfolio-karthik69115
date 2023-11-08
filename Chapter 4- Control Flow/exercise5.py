536ff78ec89a79432a2b4df30ae884013becbb6d
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 19:53:39 +0400

    Update exercise5.py

--- Chapter 4- Control Flow/exercise5.py
+++ Chapter 4- Control Flow/exercise5.py
@@ -1,4 +1,9 @@
 #favourit fruit
+# - If 'guava' is in the list, it prints "Your favorite fruit is guava!"
+# - If 'orange' is in the list, it prints "Your favorite fruit is orange!"
+# - If 'cranberries' is in the list, it prints "Your favorite fruit is cranberries!"
+# - If 'chikoo' is in the list, it prints "Your favorite fruit is chikoo!"
+# - If 'passion fruit' is in the list, it prints "Your favorite fruit is passion fruit!"
 myfavorite_fruits=['canberries', 'strawberries', 'passion fruit']
 if 'guva' in myfavorite_fruits:
     print("Your favorite fruit is guva!")
