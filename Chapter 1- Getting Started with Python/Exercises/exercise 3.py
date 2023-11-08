29fe76c9578f4f9f75cd06d0789660d05b6ba602
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 19:38:00 +0400

    Update exercise 3.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 3.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 3.py
@@ -1,4 +1,8 @@
+# Import the 'datetime' module to work with date and time information.
 import datetime
+# Get the current date and time by calling 'datetime.now()' method.
 now = datetime.datetime.now()
+# Print a header to indicate that we are displaying the current date and time.
 print ("Current date and time : ")
+# The format specified here will display the date and time in the format "YYYY-MM-DD HH:MM:SS".
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
