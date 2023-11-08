80a52346dfdeaf4788cd631c2f930433a5829ed5
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 19:28:35 +0400

    Update exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.py
@@ -1,3 +1,4 @@
+# This code prints the well-known nursery rhyme "Twinkle, Twinkle, Little Star" using triple-quoted string
 print("""Twinkle, twinkle, little star,
 	How I wonder what you are! 
 		Up above the world so high,   		
