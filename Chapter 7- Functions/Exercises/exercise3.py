5d2df04e6e9840945ed1bb56509fab85784df4e2
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 21:26:01 +0400

    Update exercise3.py

--- Chapter 7- Functions/Exercises/exercise3.py
+++ Chapter 7- Functions/Exercises/exercise3.py
@@ -1,4 +1,6 @@
-def make_shirt(shirtsize, textmessage):
+
+# Define a function called 'make_shirt' that takes two parameters: 'shirtsize' and 'textmessage'.
+# The function's docstring summarizes the shirt that's going to be made.def make_shirt(shirtsize, textmessage):
     """Summarize the shirt that's going to be made."""
     print("\nI'm going to make a "+shirtsize +" t-shirt.")
     print('It will display,"'+ textmessage+'"')
