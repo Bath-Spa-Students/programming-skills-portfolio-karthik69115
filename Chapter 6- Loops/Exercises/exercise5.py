e6fd7ab2e249f619616171643a31fd8d647bbf5f
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 21:22:56 +0400

    Update exercise5.py

--- Chapter 6- Loops/Exercises/exercise5.py
+++ Chapter 6- Loops/Exercises/exercise5.py
@@ -1,16 +1,23 @@
 #Using the list sandwich_orders from Exercise 7-8, make sure the sandwich 'pastrami' appears in the list at least three times. Add code
+# Define a list of 'sandwich_orders' that contains various sandwich types, including 'pastrami'.
 sandwich_orders = [
     'pastrami', 'cabbage', 'layered cheese', 'pastrami',
     'chicken breast', 'tomato', 'pastrami']
+    # Create an empty list to store finished sandwiches.
 finished_sandwiches = []
-print("we're so sorry for the inconvience, pastrami is out of stock today.")
+# Notify customers that "pastrami" is temporarily out of stock.
+print("We're sorry for the inconvenience, pastrami is out of stock today.")
+# Use a 'while' loop to remove all instances of 'pastrami' from 'sandwich_orders'.
 while 'pastrami' in sandwich_orders:
     sandwich_orders.remove('pastrami')
+# Print a newline to separate the notification from the preparation process.
 print("\n")
+# Process the remaining sandwich orders using a 'while' loop.
 while sandwich_orders:
     recent_sandwich = sandwich_orders.pop()
     print("I am preparing your " + recent_sandwich + " sandwich.")
     finished_sandwiches.append(recent_sandwich)
     print("\n")
+# Print messages for each completed sandwich.
 for sandwich in finished_sandwiches:
     print("I have made a " + sandwich + " sandwich.")
