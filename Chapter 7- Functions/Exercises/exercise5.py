4f81663c409b2ec1fd7e818905233b247622d387
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 21:27:38 +0400

    Update exercise5.py

--- Chapter 7- Functions/Exercises/exercise5.py
+++ Chapter 7- Functions/Exercises/exercise5.py
@@ -1,3 +1,5 @@
+# Define a function called 'describe_city' that takes two parameters: 'city' and 'country'.
+# The 'country' parameter has a default value of 'India'.
 def describe_city(city, country='India'):
     msg=city.title()+" is in "+country.title()+"."
     print(msg)
