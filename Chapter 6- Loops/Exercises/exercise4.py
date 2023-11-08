0c3bb459f6312557e69f49a05a4dc39e334bc093
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 21:21:20 +0400

    Update exercise4.py

--- Chapter 6- Loops/Exercises/exercise4.py
+++ Chapter 6- Loops/Exercises/exercise4.py
@@ -1,3 +1,7 @@
+# The 'sandwich_orders' list contains orders for different types of sandwiches, and the 'finished_sandwiches' list is initially empty.
+# It uses a 'while' loop to process sandwich orders until the 'sandwich_orders' list is empty.
+# Within the loop, it pops the most recent sandwich order, prepares it, appends it to the 'finished_sandwiches' list, and prints a message indicating that the sandwich is being prepared.
+# After all orders are processed, it uses another 'for' loop to print a message for each finished sandwich.
 sandwich_orders =['smashed potato', 'layered cheese', 'chicken breast', 'tomato']
 finished_sandwiches=[]
 while sandwich_orders:
