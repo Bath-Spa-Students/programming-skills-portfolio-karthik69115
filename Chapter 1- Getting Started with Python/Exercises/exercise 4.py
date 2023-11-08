5c67c2923626e37cba24371ad872b647971f8b29
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 19:39:07 +0400

    Update exercise 4.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 4.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 4.py
@@ -1,2 +1,3 @@
+# Define a variable 'fave_language' to store the favorite programming language, which is set to "Python."
 fave_language = "Python"
 print("I like coding in " + fave_language + " the most")
