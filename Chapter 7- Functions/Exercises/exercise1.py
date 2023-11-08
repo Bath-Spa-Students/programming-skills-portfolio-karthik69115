9982d74be36f3e66e3f9cf00db0a92a5557e1c9d
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 21:24:08 +0400

    Update exercise1.py

--- Chapter 7- Functions/Exercises/exercise1.py
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -1,3 +1,5 @@
+# Define a function named 'display_message'.
+# The function's docstring explains that it is used to display a message about what is being learned in this chapter.
 def display_message():
     """to Display a message about what I am learning in this chapter."""
     msg = "I'm learning about how to store code in functions."
