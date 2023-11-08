adbdf580a2f4495768bd24d9106e28e82bc35fe2
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 19:52:01 +0400

    Update exercise3.py

--- Chapter 4- Control Flow/exercise3.py
+++ Chapter 4- Control Flow/exercise3.py
@@ -1,4 +1,6 @@
-alien_color = 'red'
+# If the alien's color is 'green', the player earns 5 points, and a corresponding message is printed.
+# If the alien's color is 'yellow', the player earns 10 points, and a different message is printed.
+# For any other color (including 'red' in this case), the player earns 15 points, and a generic message is printed.alien_color = 'red'
 if alien_color=='green':
     print("well played, player123 has earned 5 points in the game!")
 elif alien_color=='yellow':
