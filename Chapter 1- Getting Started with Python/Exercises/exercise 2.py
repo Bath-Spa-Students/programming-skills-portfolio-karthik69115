d283baa8a1edb97e66c1a5c92450972d9b82a6a3
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 19:25:59 +0400

    Update exercise 2.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 2.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 2.py
@@ -1,5 +1,10 @@
+# Import the sys module to access system-related information
 import sys
+# Print a header to indicate that we are displaying Python version information
 print("Python version")
+# Print the full Python version string, including build and release information
 print(sys.version)
+# Print a header to indicate that we are displaying Python version info in a structured way
 print("Version info.")
+# Print the Python version information as a structured tuple
 print(sys.version_info)
