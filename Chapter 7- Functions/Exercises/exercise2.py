d8609eb8f624860f12d741859ea00814d72f82a2
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 21:25:11 +0400

    Update exercise2.py

--- Chapter 7- Functions/Exercises/exercise2.py
+++ Chapter 7- Functions/Exercises/exercise2.py
@@ -1,3 +1,5 @@
+# Define a function called 'favorite_book' that takes a 'title' as a parameter.
+# The function's docstring explains its purpose, which is to display a message about a favorite book.
 def favorite_book(title):
     """ to Display a message on favorite book."""
     print(title+" is one of my most favorite books.")
