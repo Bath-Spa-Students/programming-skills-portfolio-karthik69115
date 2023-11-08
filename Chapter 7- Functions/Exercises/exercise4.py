d8ce152848572f799032d0078d66d90ec1e5f437
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 21:26:51 +0400

    Update exercise4.py

--- Chapter 7- Functions/Exercises/exercise4.py
+++ Chapter 7- Functions/Exercises/exercise4.py
@@ -1,3 +1,5 @@
+# Define a function called 'make_shirt' with two optional parameters, 'shirtsize' and 'textmessage'.
+# The function's default values are set to 'large' for the shirt size and 'I love Python!' for the text message.
 def make_shirt(shirtsize='large', textmessage='I love Python!'):
     print("\nI'm making a "+shirtsize+" t-shirt.")
     print('It will display, "'+textmessage+'"')
