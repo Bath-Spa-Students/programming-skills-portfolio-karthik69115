271f08ff097741de4e26809e80c67120ee06fb7d
karthik69115 <144330437+karthik69115@users.noreply.github.com>
2023-11-08 21:20:08 +0400

    Update exercise2.py

--- Chapter 6- Loops/Exercises/exercise2.py
+++ Chapter 6- Loops/Exercises/exercise2.py
@@ -1,4 +1,8 @@
 #A movie theater charges different ticket prices depending on a person’s age. If a person is under the age of 3, the ticket is free
+# If the user enters an age, it converts the input to an integer and checks the age to determine the ticket cost.
+# - If the age is less than 3, it prints that the person is allowed to enter for free.
+# - If the age is between 3 and 12 (inclusive), it prints that the ticket will cost $10.
+# - For all other ages, it prints that the ticket will cost $15.
 prompt="what is your age?"
 prompt+="\nEnter 'quit' when you wish to stop."
 while True:
